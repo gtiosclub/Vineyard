d8c920b6c87a208ad4da1c88bb92369651c258b3
Jin Lee <yjlpo2408@gmail.com>
2024-09-12 15:36:08 -0400

    Update LoginView.swift
    dict for testing
    **//temp marked codes could be deleted after -- was for testing**

--- Vineyard/Vineyard/LoginView.swift
+++ Vineyard/Vineyard/LoginView.swift
@@ -12,10 +12,18 @@ struct LoginView: View {
     @State private var password: String = ""
     @Binding var isAuthenticated: Bool
     
+    @State private var showAlert = false //temp
+    @State private var alertMessage = "" //temp
+    
+    //dict
+    let userCred: [String: String] = [
+        "Test1": "pass123",
+        "Test2": "pass456",
+    ]
+    
     var body: some View {
         VStack(spacing: 20) {
-            
-            
+            //title goes here
             
             TextField("Enter username", text: $username)
                 .padding()
@@ -43,20 +51,28 @@ struct LoginView: View {
             NavigationLink(destination: ResetPasswordView()) {
                 Text("Forgot Password?")
             }
-
             
         }
-
+        
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text(alertMessage))
+        } //temp
+        
     }
-
-                    func authenticateUser() {
-                        if username == "TestUser" && password == "Password123" {
-                            isAuthenticated = true
-                        } else {
-                            isAuthenticated = false
-                        }
-                    }
-                }
+    
+    func authenticateUser() {
+        if let storedPassword = userCred[username], storedPassword == password {
+            isAuthenticated = true
+            
+            alertMessage = "Login Successful" //temp
+        } else {
+            isAuthenticated = false
+            alertMessage = "Login Failed" //temp
+        }
+        
+        showAlert = true //temp
+    }
+}
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
