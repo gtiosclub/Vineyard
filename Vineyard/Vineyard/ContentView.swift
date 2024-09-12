62cc86549809fa14d371c844b10206ae80f72058
Yashasvi Karri <yashasvikarri@Yashasvis-MacBook-Pro.local>
2024-09-12 16:15:47 -0400

    Did model and base to-do UI

--- Vineyard/Vineyard/ContentView.swift
+++ Vineyard/Vineyard/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(ViewModel.self) private var viewModel
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            VStack {
+                Text("\(viewModel.user.name)")
+                List(viewModel.user.participatingGroups) { group in
+                    // TODO: Use a NavigationLink to select a list.
+                    NavigationLink(group.groupName) {
+                        ResolutionView(group: group)
+                    }
+                }
+            }
+            
+        }.navigationTitle("Group Menu")
+
     }
 }
 
---
+++ Vineyard/Vineyard/CreateResolutionView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateResolutionView.swift
+//  Vineyard
+//
+//  Created by Yashasvi Karri on 9/12/24.
+//
+
+import SwiftUI
+
+struct CreateResolutionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateResolutionView()
+}
