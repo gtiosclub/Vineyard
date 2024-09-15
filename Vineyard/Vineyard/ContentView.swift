74273ad1567c585e7f95cf40199c42750b97bb90
sacchit25 <sacchitmittal@gmail.com>
2024-09-15 15:04:30 -0400

    replcaed hstack with toolbar

--- Vineyard/Vineyard/ContentView.swift
+++ Vineyard/Vineyard/ContentView.swift
@@ -12,28 +12,27 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                HStack {
-                    Spacer()
-                    Text("\(viewModel.user.name)")
-                        .frame(maxWidth: 275, alignment: .center)
-                    NavigationLink(destination: ProfileView()) {
-                        Image(systemName: "person.crop.circle.fill")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                            .padding(.trailing, 22)
-                            .foregroundColor(.black)
-                    }
-                }
                 List(viewModel.user.participatingGroups) { group in
                     // TODO: Use a NavigationLink to select a list.
                     NavigationLink(group.groupName) {
                         ResolutionView(group: group)
                     }
                 }
+            }.toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("\(viewModel.user.name)")
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: ProfileView()) {
+                        Image(systemName: "person.crop.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.black)
+                    }
+                }
             }
             
         }.navigationTitle("Group Menu")
-
     }
 }
 
