bbbeeabdef23748be019e8967c511c042df619cc
Tony Nguyen <tonywins35@gmail.com>
2024-09-15 17:02:10 -0400

    login model and viewmodel

---
+++ Vineyard/Vineyard/LoginModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginModel.swift
+//  Vineyard
+//
+//  Created by Tony Nguyen on 9/15/24.
+//
+
+import Foundation
---
+++ Vineyard/Vineyard/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  Vineyard
+//
+//  Created by Tony Nguyen on 9/15/24.
+//
+
+import Foundation
