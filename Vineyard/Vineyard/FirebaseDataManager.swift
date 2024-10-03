f9735961f1abfbce626d321c5686e2389b610bf4
Jin Lee <97980723+hiimjin@users.noreply.github.com>
2024-10-03 19:14:41 -0400

    basic screen and view model for the dashboard (#29)
    * dashboard UI and widget enum
    
    * dashboard widgets

--- Vineyard/Vineyard/FirebaseDataManager.swift
+++ Vineyard/Vineyard/FirebaseDataManager.swift
@@ -7,31 +7,31 @@
 
 // import FirebaseFirestore
 
-class FirebaseDataManager: DatabaseServiceProtocol {
-    
-    func fetchGroupFromDB(groupID: String) async throws -> Group? {
-        <#code#>
-    }
-    
-    func fetchPeopleFromDB(peopleIDs: [String]) async throws -> [Person] {
-        <#code#>
-    }
-    
-    func fetchResolutionsFromDB(resolutionIDs: [String]) async throws -> [Resolution] {
-        <#code#>
-    }
-    
-    func addPersonToDB(person: Person) async throws {
-        <#code#>
-    }
-    
-    func addResolutionToDB(resolution: Resolution) async throws {
-        <#code#>
-    }
-    
-    func addGroupToDB(group: Group) async throws {
-        <#code#>
-    }
-    
-    
-}
+//class FirebaseDataManager: DatabaseServiceProtocol {
+//    
+//    func fetchGroupFromDB(groupID: String) async throws -> Group? {
+//        <#code#>
+//    }
+//    
+//    func fetchPeopleFromDB(peopleIDs: [String]) async throws -> [Person] {
+//        <#code#>
+//    }
+//    
+//    func fetchResolutionsFromDB(resolutionIDs: [String]) async throws -> [Resolution] {
+//        <#code#>
+//    }
+//    
+//    func addPersonToDB(person: Person) async throws {
+//        <#code#>
+//    }
+//    
+//    func addResolutionToDB(resolution: Resolution) async throws {
+//        <#code#>
+//    }
+//    
+//    func addGroupToDB(group: Group) async throws {
+//        <#code#>
+//    }
+//    
+//    
+//}
