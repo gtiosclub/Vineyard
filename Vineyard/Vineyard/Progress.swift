7fd65023eb0476fb119b95619a1c047f72ede06d
Lee, Jiyoon <jlee3857@gatech.edu>
2024-10-08 19:11:47 -0400

    resolution reconstructed. accessing enum using struct

--- Vineyard/Vineyard/Progress.swift
+++ Vineyard/Vineyard/Progress.swift
@@ -32,18 +32,18 @@ struct Progress: Identifiable {
         var vishnesh = Person(name: "Vishnesh", email: "v@aol.com")
         
         let resolution1 = Resolution.samples[0]
-        let progress1 = Progress(resolution: resolution1, quantityGoal: 1, frequencyGoal: Frequency.weekly(count: 3), person: andrew)
+        let progress1 = Progress(resolution: resolution1, quantityGoal: 1, frequencyGoal: Frequency(frequencyType: FrequencyType.weekly, count: 3), person: andrew)
         
-        let progress2 = Progress(resolution: resolution1, quantityGoal: 1, frequencyGoal: Frequency.weekly(count: 3), person: yash)
+        let progress2 = Progress(resolution: resolution1, quantityGoal: 1, frequencyGoal: Frequency(frequencyType: FrequencyType.weekly, count: 3), person: yash)
         
-        let progress3 = Progress(resolution: resolution1, quantityGoal: 1, frequencyGoal: Frequency.weekly(count: 3), person: sankaet)
+        let progress3 = Progress(resolution: resolution1, quantityGoal: 1, frequencyGoal: Frequency(frequencyType: FrequencyType.weekly, count: 3), person: sankaet)
         
         let resolution2 = Resolution.samples[1]
-        let progress4 = Progress(resolution: resolution2, quantityGoal: 1, frequencyGoal: Frequency.weekly(count: 3), person: andrew)
+        let progress4 = Progress(resolution: resolution2, quantityGoal: 1, frequencyGoal: Frequency(frequencyType: FrequencyType.weekly, count: 3), person: andrew)
         
-        let progress5 = Progress(resolution: resolution2, quantityGoal: 1, frequencyGoal: Frequency.weekly(count: 3), person: yash)
+        let progress5 = Progress(resolution: resolution2, quantityGoal: 1, frequencyGoal: Frequency(frequencyType: FrequencyType.weekly, count: 3), person: yash)
         
-        let progress6 = Progress(resolution: resolution2, quantityGoal: 1, frequencyGoal: Frequency.weekly(count: 3), person: sankaet)
+        let progress6 = Progress(resolution: resolution2, quantityGoal: 1, frequencyGoal: Frequency(frequencyType: FrequencyType.weekly, count: 3), person: sankaet)
         
         return [progress1, progress2, progress3, progress4, progress5, progress6]
     }
