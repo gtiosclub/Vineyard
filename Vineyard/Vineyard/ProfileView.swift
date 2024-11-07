837aad58cbdfe4de7259c74ffd8f500eb80e5bcd
sacchit25 <sacchitmittal@gmail.com>
2024-11-06 23:28:52 -0500

    storing b4 merging main

--- Vineyard/Vineyard/ProfileView.swift
+++ Vineyard/Vineyard/ProfileView.swift
@@ -17,46 +17,39 @@ struct ProfileView: View {
                 VStack {
                     Button(action:{
                         Task {
-                            await loginViewModel.signOut()
+//                            await loginViewModel.signOut()
                         }
                     }) {
                         Text("Sign out")
                     }
-                    HStack(spacing: 20) {
-                        Image(systemName: "person.crop.circle.fill")
-                            .resizable()
-                            .frame(width: 70, height: 70)
-                            .foregroundColor(.gray)
+//                    HStack(spacing: 20) {
                         VStack(alignment: .leading, spacing: 2) {
-                            Text(loginViewModel.currentUser?.name ?? "no name")
-                                .font(.system(size: 20, weight: .bold))
+//                            Text(loginViewModel.currentUser?.name ?? "no name")
+                            Text("HI")
+                                .font(.system(size: 32, weight: .bold))
+                                .foregroundColor(Color.profileViewInfo)
                             HStack() {
                                 let count = viewModel.user.groups.count
                                 Text("\(count) \(count > 1 ? "groups" : "group")")
                                      .font(.system(size: 16))
                                      .fontWeight(.regular)
-                                     .foregroundColor(.gray)
+                                     .foregroundColor(Color.profileViewInfo)
                             }
                         }
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding()
-                    .background(Color(red: 0.937, green: 0.937, blue: 0.937))
-                    .cornerRadius(20)
-                    
-                    Text("Winery")
                         .frame(maxWidth: .infinity, alignment: .leading)
-                        .font(.system(size: 20, weight: .bold))
-                        .padding(.top, 44)
+                    }
                     
                     LazyVGrid(columns: columns, spacing: 10) {
                         ForEach(viewModel.user.badges) { badge in
                             BadgeView(badge: badge)
                         }
                     }
+                    .padding(.top, 17)
                 }
                 .padding(.horizontal, 20)
-            } .toolbar {
+                .background(Color.profileViewCellBackground)
+            }
+            .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
                     Text("Profile")
                         .font(.system(size: 32, weight: .bold))
@@ -64,7 +57,7 @@ struct ProfileView: View {
             }
         }
     }
-}
+
 
 struct BadgeView: View {
     let badge: Badge
@@ -75,9 +68,10 @@ struct BadgeView: View {
                 .resizable()
                 .frame(width: 40, height: 140)
                 .padding()
+            Text("\(badge.dateObtained)")
         }
         .frame(width: 110, height: 200)
-        .background(Color(red: 0.937, green: 0.937, blue: 0.937))
+        .background(Color.white)
         .cornerRadius(20)
     }
 }
