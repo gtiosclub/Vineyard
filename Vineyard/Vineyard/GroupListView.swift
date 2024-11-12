256ed71e6c5516d9ccb871e28ac8f60b0d391f31
Sankaet Cheemalamarri <133285341+sankaet8@users.noreply.github.com>
2024-11-11 19:12:54 -0500

    Fixed the GroupListView UI (#81)
    * Fixed a few bugs causing the app to crash
    
    * Fixed UI bug on Group list view

--- Vineyard/Vineyard/GroupListView.swift
+++ Vineyard/Vineyard/GroupListView.swift
@@ -15,38 +15,38 @@ struct GroupListView: View {
     @State private var isPresentingAddGroup = false
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack(alignment: .center, spacing: 30) {
-                    Spacer()
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("Your Groups")
-                                .font(.system(size: 30, weight: .bold))
-                                .foregroundColor(.white)
-                            Text("\(viewModel.user?.groupIDs.count ?? 0) active group(s)")
-                                .foregroundColor(.white)
-                            
-                        }
-                        Spacer()
-                        Button(action: {
-                            print("changing mode")
-                        }) {
-                            Image(systemName: "list.bullet")
-                                .foregroundColor(.white)
-                                .font(.system(size: 25))
-                        }
-                        Button(action: {
-                            print("add")
-                            viewModel.isPresentingCreateGroupView = true
-                        }) {
-                            Image(systemName: "plus")
-                                .foregroundColor(.white)
-                                .font(.system(size: 25))
-                        }
+            VStack(alignment: .center, spacing: 30) {
+                Spacer()
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Your Groups")
+                            .font(.system(size: 30, weight: .bold))
+                            .foregroundColor(.white)
+                        Text("\(viewModel.user?.groupIDs.count ?? 0) active group(s)")
+                            .foregroundColor(.white)
+                        
                     }
-                    .padding(40)
-                    
                     Spacer()
+                    Button(action: {
+                        print("changing mode")
+                    }) {
+                        Image(systemName: "list.bullet")
+                            .foregroundColor(.white)
+                            .font(.system(size: 25))
+                    }
+                    Button(action: {
+                        print("add")
+                        viewModel.isPresentingCreateGroupView = true
+                    }) {
+                        Image(systemName: "plus")
+                            .foregroundColor(.white)
+                            .font(.system(size: 25))
+                    }
+                }
+                .padding(40)
+                
+                Spacer()
+                ScrollView {
                     VStack(spacing: 5) {
                         ForEach(viewModel.groups, id: \.id) { group in
                             NavigationLink(destination: GroupView(group: group)) {
@@ -56,11 +56,11 @@ struct GroupListView: View {
                         }.padding()
                     }
                 }
-                .navigationBarHidden(true)
-                .background(alignment: .top) {
-                    Image("topBackground")
-                }
-            }.ignoresSafeArea()
+            }
+            .navigationBarHidden(true)
+            .background(alignment: .top) {
+                Image("topBackground")
+            }.ignoresSafeArea(.container, edges: .top)
         }
         .fullScreenCover(isPresented: $viewModel.isPresentingCreateGroupView) {
             CreateGroupView().environment(viewModel)
