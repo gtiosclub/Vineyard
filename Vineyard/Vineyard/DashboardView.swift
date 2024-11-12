384906ccffd78f854d491f341228732c2e6e3fba
vishneshjramanathan@gmail.com <vishneshjramanathan@gmail.com>
2024-11-11 23:17:56 -0500

    Take dashboard title out of scroll view

--- Vineyard/Vineyard/DashboardView.swift
+++ Vineyard/Vineyard/DashboardView.swift
@@ -20,100 +20,99 @@ struct DashboardView: View {
     @EnvironmentObject var inviteViewModel: InviteViewModel
    
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 10) {
-                // Heading
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Dashboard")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-
-                        Text(Date.now, style: .date)
-                            .font(.subheadline)
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-
-                    Image(profileImage)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 50, height: 50)
-                        .clipShape(Circle())
-                        .overlay(
-                            Circle()
-                                .stroke(Color.white, lineWidth: 5)
-                                .opacity(0.5)
-                        ) // Placeholder; erase after profile picture added
+        VStack(alignment: .leading, spacing: 10) {
+            // Heading
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Dashboard")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    Text(Date.now, style: .date)
+                        .font(.subheadline)
+                        .foregroundColor(.white)
                 }
-                .padding([.horizontal, .top])
-                .padding(.top, 10)
-                .padding(.bottom, 30)
-
+                Spacer()
+                
+                Image(profileImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 50, height: 50)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle()
+                            .stroke(Color.white, lineWidth: 5)
+                            .opacity(0.5)
+                    ) // Placeholder; erase after profile picture added
+            }
+            .padding([.horizontal, .top])
+            .padding(.top, 10)
+            .padding(.bottom, 30)
+            
+            ScrollView {
                 // Grid for Widgets
                 LazyVGrid(
-                   columns: [
-                       GridItem(.adaptive(minimum: UIScreen.main.bounds.width / 2 - 30))
-                   ],
-                   spacing: 40
-               ) {
-                   ForEach(Array(widgets.enumerated()), id: \.offset) { index, widget in
-                       if widget.span == 2 {
-                           // Full-width
-                           Section {
-                               ZStack(alignment: .topTrailing) {
-                                   widget.render()
-                                       .frame(width: UIScreen.main.bounds.width * 0.9, height: 180)
-                                       .offset(x: 93)
-                                   
-                                   if widget.title == "Recent Activities" {
-                                       let taskCount = getTaskCount(for: widget)
-                                       if taskCount > 0 {
-                                           Circle()
-                                               .fill(Color.red)
-                                               .frame(width: 30, height: 30)
-                                               .overlay(
-                                                   Text("\(taskCount)")
-                                                       .font(.body)
-                                                       .foregroundColor(.white)
-                                               )
-                                               .offset(x: 105, y: -20)
-                                       }
-                                   }
-                               }
-                               .frame(maxWidth: .infinity)
-                           }
-                       } else {
-                           // Half-width
-                           ZStack(alignment: .topTrailing) {
-                               widget.render()
-                                   .frame(height: 180)
-                               
-                               if widget.title == "Recent Activities" {
-                                   let taskCount = getTaskCount(for: widget)
-                                   if taskCount > 0 {
-                                       Circle()
-                                           .fill(Color.red)
-                                           .frame(width: 30, height: 30)
-                                           .overlay(
-                                               Text("\(taskCount)")
-                                                   .font(.body)
-                                                   .foregroundColor(.white)
-                                           )
-                                           .offset(x: 10, y: -20)
-                                   }
-                               }
-                           }
-                           .frame(maxHeight: 180)
-                           .frame(width: UIScreen.main.bounds.width * 0.43)
-                       }
-                   }
-               }
-               .padding()
-            }
+                    columns: [
+                        GridItem(.adaptive(minimum: UIScreen.main.bounds.width / 2 - 30))
+                    ],
+                    spacing: 40
+                ) {
+                    ForEach(Array(widgets.enumerated()), id: \.offset) { index, widget in
+                        if widget.span == 2 {
+                            // Full-width
+                            Section {
+                                ZStack(alignment: .topTrailing) {
+                                    widget.render()
+                                        .frame(width: UIScreen.main.bounds.width * 0.9, height: 180)
+                                        .offset(x: 93)
+                                    
+                                    if widget.title == "Recent Activities" {
+                                        let taskCount = getTaskCount(for: widget)
+                                        if taskCount > 0 {
+                                            Circle()
+                                                .fill(Color.red)
+                                                .frame(width: 30, height: 30)
+                                                .overlay(
+                                                    Text("\(taskCount)")
+                                                        .font(.body)
+                                                        .foregroundColor(.white)
+                                                )
+                                                .offset(x: 105, y: -20)
+                                        }
+                                    }
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        } else {
+                            // Half-width
+                            ZStack(alignment: .topTrailing) {
+                                widget.render()
+                                    .frame(height: 180)
+                                
+                                if widget.title == "Recent Activities" {
+                                    let taskCount = getTaskCount(for: widget)
+                                    if taskCount > 0 {
+                                        Circle()
+                                            .fill(Color.red)
+                                            .frame(width: 30, height: 30)
+                                            .overlay(
+                                                Text("\(taskCount)")
+                                                    .font(.body)
+                                                    .foregroundColor(.white)
+                                            )
+                                            .offset(x: 10, y: -20)
+                                    }
+                                }
+                            }
+                            .frame(maxHeight: 180)
+                            .frame(width: UIScreen.main.bounds.width * 0.43)
+                        }
+                    }
+                }
+                .padding()
+            }.scrollIndicators(.hidden)
         }
-        .scrollIndicators(.hidden)
         .background(
             LinearGradient(
                 gradient: Gradient(colors: [
@@ -137,7 +136,7 @@ struct DashboardView: View {
         .alert(isPresented: $inviteViewModel.inviteErrorStatus) {
             Alert(title: Text(inviteViewModel.inviteError ?? ""))
         }
-        
+    
     }
     private func getTaskCount(for widget: Widget) -> Int {
         if let recentActivitiesFull = widget as? RecentActivitiesWidgetFull {
