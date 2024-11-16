cab33f8b50c7db9fe048f986a0afbdab51b480cc
Jin Lee <yjlpo2408@gmail.com>
2024-11-16 13:45:49 -0500

    Update DashboardView.swift

--- Vineyard/Vineyard/DashboardView.swift
+++ Vineyard/Vineyard/DashboardView.swift
@@ -18,58 +18,28 @@ struct DashboardView: View {
 
     private let profileImage = "profile_image"
     @EnvironmentObject var inviteViewModel: InviteViewModel
-   
+
     var body: some View {
-        NavigationStack {
-            VStack(alignment: .leading, spacing: 10) {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Dashboard")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                        
-                        Text(Date.now, style: .date)
-                            .font(.subheadline)
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-                    
-                    NavigationLink(destination: ProfileView()) {
-                        Image(profileImage)
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 50, height: 50)
-                            .clipShape(Circle())
-                            .overlay(
-                                Circle()
-                                    .stroke(Color.white, lineWidth: 5)
-                                    .opacity(0.5)
-                            ) // Placeholder; erase after profile picture added
-                        
-                    }
-                }
-                .padding([.horizontal, .top])
-                .padding(.top, 10)
-                .padding(.bottom, 30)
-                
-                ScrollView {
+        ZStack(alignment: .top) {
+            ScrollView {
+                VStack {
+                    Spacer().frame(height: 135)
                     // Grid for Widgets
                     LazyVGrid(
                         columns: [
-                            GridItem(.adaptive(minimum: UIScreen.main.bounds.width / 2 - 30))
+                            GridItem(.adaptive(minimum: UIScreen.main.bounds.width * 0.4))
                         ],
-                        spacing: 40
+                        spacing: 37
                     ) {
                         ForEach(Array(widgets.enumerated()), id: \.offset) { index, widget in
                             if widget.span == 2 {
-                                // Full-width
+                                // Full-width widget
                                 Section {
                                     ZStack(alignment: .topTrailing) {
                                         widget.render()
-                                            .frame(width: UIScreen.main.bounds.width * 0.9, height: 180)
-                                            .offset(x: 93)
-                                        
+                                            .frame(width: UIScreen.main.bounds.width * 0.91, height: 180)
+                                            .offset(x: 96)
+
                                         if widget.title == "Recent Activities" {
                                             let taskCount = getTaskCount(for: widget)
                                             if taskCount > 0 {
@@ -81,18 +51,18 @@ struct DashboardView: View {
                                                             .font(.body)
                                                             .foregroundColor(.white)
                                                     )
-                                                    .offset(x: 105, y: -20)
+                                                    .offset(x: 110, y: -20)
                                             }
                                         }
                                     }
                                     .frame(maxWidth: .infinity)
                                 }
                             } else {
-                                // Half-width
+                                // Half-width widget
                                 ZStack(alignment: .topTrailing) {
                                     widget.render()
                                         .frame(height: 180)
-                                    
+
                                     if widget.title == "Recent Activities" {
                                         let taskCount = getTaskCount(for: widget)
                                         if taskCount > 0 {
@@ -104,44 +74,67 @@ struct DashboardView: View {
                                                         .font(.body)
                                                         .foregroundColor(.white)
                                                 )
-                                                .offset(x: 10, y: -20)
+                                                .offset(x: 13, y: -20)
                                         }
                                     }
                                 }
-                                .frame(maxHeight: 180)
-                                .frame(width: UIScreen.main.bounds.width * 0.43)
+                                .frame(width: UIScreen.main.bounds.width * 0.44)
                             }
                         }
                     }
                     .padding()
+                    .padding(.horizontal, 11)
                 }
-                .scrollIndicators(.hidden)
             }
-            .background(
-                LinearGradient(
-                    gradient: Gradient(colors: [
-                        Color(red: 163 / 255, green: 123 / 255, blue: 198 / 255),
-                        Color(red: 163 / 255, green: 123 / 255, blue: 198 / 255).opacity(0.7),
-                        Color.gray.opacity(0.1)
-                    ]),
-                    startPoint: .top,
-                    endPoint: .center
-                )
-                .edgesIgnoringSafeArea(.all)
-            )
-            .popup(isPresented: $inviteViewModel.invitedToGroup) {
-                InvitePopupView()
-            } customize: {
-                $0
-                    .type(.floater())
-                    .appearFrom(.bottomSlide)
-                
+            .scrollIndicators(.hidden)
+
+            Image("topBackground")
+                .ignoresSafeArea(.container, edges: .top)
+                .zIndex(1)
+
+            VStack(alignment: .leading, spacing: 10) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Dashboard")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+
+                        Text(Date.now, style: .date)
+                            .font(.subheadline)
+                            .foregroundColor(.white)
+                    }
+                    Spacer().frame(width:145)
+                    
+
+                    Image(profileImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 50, height: 50)
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle()
+                                .stroke(Color.white, lineWidth: 5)
+                                .opacity(0.5)
+                        )
+                }
+                .padding(.top, 15)
             }
-            .alert(isPresented: $inviteViewModel.inviteErrorStatus) {
-                Alert(title: Text(inviteViewModel.inviteError ?? ""))
-            }
+            .zIndex(2)
+        }
+        .navigationBarHidden(true)
+        .popup(isPresented: $inviteViewModel.invitedToGroup) {
+            InvitePopupView()
+        } customize: {
+            $0
+                .type(.floater())
+                .appearFrom(.bottomSlide)
+        }
+        .alert(isPresented: $inviteViewModel.inviteErrorStatus) {
+            Alert(title: Text(inviteViewModel.inviteError ?? ""))
         }
     }
+
     private func getTaskCount(for widget: Widget) -> Int {
         if let recentActivitiesFull = widget as? RecentActivitiesWidgetFull {
             return recentActivitiesFull.recentActivities.count
@@ -154,4 +147,5 @@ struct DashboardView: View {
 
 #Preview {
     DashboardView()
+        .environmentObject(InviteViewModel())
 }
