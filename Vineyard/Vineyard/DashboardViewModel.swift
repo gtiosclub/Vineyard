8acad0dead90800470804637432621120ca0732d
JosheevR <josheev@gmail.com>
2024-11-17 20:49:39 -0500

    fix navigation from dashboard to profile

--- Vineyard/Vineyard/DashboardViewModel.swift
+++ Vineyard/Vineyard/DashboardViewModel.swift
@@ -8,4 +8,5 @@
 import SwiftUI
 
 class DashboardViewModel: ObservableObject {
+    
 }
--- Vineyard/Vineyard/GroupView.swift
+++ Vineyard/Vineyard/GroupView.swift
@@ -170,59 +170,10 @@ struct GroupView: View {
                                 RoundedRectangle(cornerRadius: 20)
                                     .foregroundStyle(.ultraThinMaterial)
                             }
-                            
-                            
                         }
                     }
                     
-                    
-                    
-                    //                    Text("Recent Activities")
-                    //                        .font(.headline)
-                    //                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    
-                    //                    ForEach(group.people) {people in
-                    //                        VStack(alignment: .leading, spacing: 10) {
-                    //                            HStack {
-                    //                                Image(systemName: "person.crop.circle")
-                    //                                    .resizable()
-                    //                                    .frame(width: 30, height: 30)
-                    //                                Text("\(people.name)")
-                    //                                Spacer()
-                    //
-                    //                                VStack {
-                    //                                    Text("Lorem Ipsum")
-                    //                                        .font(.subheadline)
-                    //                                    Text("Lorem Ipsum")
-                    //                                        .font(.caption)
-                    //                                        .foregroundColor(Color.black.opacity(0.5))
-                    //                                }
-                    //                            }
-                    //                        }
-                    //                        .padding()
-                    //                        .frame(height: 60)
-                    //                        .background(Color.gray.opacity(0.5))
-                    //                        .cornerRadius(10)
-                    //                    }
-                    
                 }
-                .padding(.horizontal, 20)
-            }
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Text("\(group.name)")
-                        .font(.system(size: 24, weight: .bold))
-
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(8)
-                .background {
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(Color.purple.opacity(0.2))
-                }
-                
-                
             }
             .padding(.horizontal, 20)
         }
