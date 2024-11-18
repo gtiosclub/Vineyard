054c31ddafeda4f1cf1ad6c0be2b662ca39eda84
Cheemalamarri, Srisankaet <scheemal3@gatech.edu>
2024-11-17 21:13:02 -0500

    fixed merge conflict

--- Vineyard/Vineyard/GroupView.swift
+++ Vineyard/Vineyard/GroupView.swift
@@ -177,7 +177,7 @@ struct GroupView: View {
                             }
                         }
                     }
-                }
+//                }
             }
             
 //            .toolbar {
